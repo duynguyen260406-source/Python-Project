e72db30ed67bc9c63ef4e19092c0b96f95ba3e82
duy <duynguyen260406@gmail.com>
2025-11-22 15:27:53 +0700

    change download data

--- download_data.py
+++ download_data.py
@@ -17,19 +17,27 @@ ARTIFACT_FILES = {
 DATA_FILES = {
     "food_calories_vi.csv": "https://drive.google.com/uc?id=19Cs9GrRDRaLliacL1CFUiR52135D7ljn",
     "exercise_dataset (1).csv": "https://drive.google.com/uc?id=12wivBtIZl3L0D-2bFn10BCF9SoMnIHEx",
-    "train.csv": "https://drive.google.com/uc?id=1QCkHkERxJQ7kwd_TbwWBrLPXAXVDH6fF",
-    "RAW_recipes.csv": "https://drive.google.com/uc?id=1NWb6zX6Pz52o_yBaZST2VvuiiW6gsRRa"
+}
+
+RAW_FILES = {
+        "train.csv": "https://drive.google.com/uc?id=1QCkHkERxJQ7kwd_TbwWBrLPXAXVDH6fF",
+        "RAW_recipes.csv": "https://drive.google.com/uc?id=1NWb6zX6Pz52o_yBaZST2VvuiiW6gsRRa"
 }
 
 def ensure_files():
     for fname, url in ARTIFACT_FILES.items():
         path = os.path.join(ARTIFACT_DIR, fname)
         if not os.path.exists(path):
-            print(f"Downloading {fname} ...")
             gdown.download(url, path, quiet=False)
 
     for fname, url in DATA_FILES.items():
         path = os.path.join(DATA_DIR, fname)
         if not os.path.exists(path):
-            print(f"Downloading {fname} ...")
+            gdown.download(url, path, quiet=False)
+
+if __name__ == "__main__":
+    ensure_files()
+    for fname, url in RAW_FILES.items():
+        path = os.path.join(DATA_DIR, fname)
+        if not os.path.exists(path):
             gdown.download(url, path, quiet=False)
